3a8399c3f0c8b716bb87846aed228e308b103eeb
araf821 <araf.ahmed200@gmail.com>
2023-01-19 20:05:49 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Goal: A flash card app that shows French words with its translation
+# in English on the other side. The purpose of this app is just to
+# help us learn some of the most commonly used French words.
+
+from tkinter import *
